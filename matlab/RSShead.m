34ef04dc46014097e1a95bd2bf3a635f2cbc459e
Wiktor Weibull <wiktor.weibull@gmail.com>
2020-10-21 09:33:17 +0200

    Simplified RSShead.m for Data types it creates the coordinates

--- matlab/RSShead.m
+++ matlab/RSShead.m
@@ -1,11 +1,17 @@
-function head = RSShead(data)
+function head = RSShead(data, type)
 % RSShead : Creates a header for a RSS formatted file
 %
 % Call :
-% head = RSShead(data);
+% head = RSShead(data, type=0);
 %
 %   Author(s): W. Weibull, 10/09-17
 %   Copyright 2017 Wiktor Weibull
+
+if (nargin < 2)
+    type=1;
+else
+    head.type=type;
+end
 
 MAXDIMS = 9;
 head.geometry.N = zeros(MAXDIMS,1);
@@ -16,7 +22,7 @@ Dsz = size(data);
 
 head.data_format = 4;
 head.header_format = 4;
-head.type = 1;
+
 head.Nheader = 0;
 head.Ndims = 1;
 
@@ -30,4 +36,27 @@ end
 
 for i=1:length(Dsz)
     head.geometry.O(i) = 0.0;
+end
+
+
+if (type==2)
+    head.Nheader=4;
+   for i=1:Dsz(2)
+       head.coordinates(i).s(1) = 0;
+       head.coordinates(i).s(2) = 0;
+       head.coordinates(i).g(1) = 0;
+       head.coordinates(i).g(2) = 0;
+   end
+end
+
+if (type==3)
+    head.Nheader=6;
+   for i=1:Dsz(2)
+       head.coordinates(i).s(1) = 0;
+       head.coordinates(i).s(2) = 0;
+       head.coordinates(i).s(3) = 0;
+       head.coordinates(i).g(1) = 0;
+       head.coordinates(i).g(2) = 0;
+       head.coordinates(i).g(3) = 0;
+   end
 end
