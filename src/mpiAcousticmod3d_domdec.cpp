86f9ba26fbd6fb80d9c146a236893cd7bf4ae7db
Wiktor Weibull <wiktorweibull@Wiktors-MacBook-Pro.local>
2021-01-29 11:59:58 +0100

    Adding domain decomposition to 3D Acoustic modelling

--- src/mpiAcousticmod3d_domdec.cpp
+++ src/mpiAcousticmod3d_domdec.cpp
@@ -27,7 +27,7 @@ int main(int argc, char** argv) {
 
     if(argc < 2){
         if(mpi.getRank() == 0){
-            PRINT_DOC(# MPI 2d acoustic modelling default configuration file);
+            PRINT_DOC(# MPI 3d acoustic modelling default configuration file);
             PRINT_DOC();
             PRINT_DOC(# Domain decomposition parameter);
             PRINT_DOC(        ndomain = "1";  # Number of domains to split the model into);
@@ -39,25 +39,30 @@ int main(int argc, char** argv) {
             PRINT_DOC(            snapinc = "10"; # Snap interval in multiples of modelling interval);
             PRINT_DOC(            dtrec = "4e-3"; # Recording interval in seconds);
             PRINT_DOC(            apertx = "900"; # Aperture for local model (source is in the middle));
+            PRINT_DOC(            aperty = "900"; # Aperture for local model (source is in the middle));
             PRINT_DOC();
             PRINT_DOC(# Booleans);
             PRINT_DOC(            Precord = "true";  # Set these to true if recording or snapshoting is to be made.);
-            PRINT_DOC(            Axrecord = "true";);
-            PRINT_DOC(        Azrecord = "true";);
+            PRINT_DOC(            Axrecord = "false";);
+            PRINT_DOC(            Ayrecord = "false";);
+            PRINT_DOC(        Azrecord = "false";);
             PRINT_DOC(        Psnap = "false";);
             PRINT_DOC(        Axsnap = "false";);
+            PRINT_DOC(        Aysnap = "false";);
             PRINT_DOC(        Azsnap = "false";);
             PRINT_DOC();
             PRINT_DOC(# Files);
-            PRINT_DOC(        Vp = "Vp2d.rss";);
-            PRINT_DOC(        Rho = "Rho2d.rss";);
-            PRINT_DOC(        Wavelet = "Wav2d.rss";);
-            PRINT_DOC(        Survey = "2DSurvey.rss";);
+            PRINT_DOC(        Vp = "Vp3d.rss";);
+            PRINT_DOC(        Rho = "Rho3d.rss";);
+            PRINT_DOC(        Wavelet = "Wav3d.rss";);
+            PRINT_DOC(        Survey = "3DSurvey.rss";);
             PRINT_DOC(        Precordfile = "Pshot.rss";);
             PRINT_DOC(        Axrecordfile = "Axshot.rss";);
+            PRINT_DOC(        Ayrecordfile = "Ayshot.rss";);
             PRINT_DOC(        Azrecordfile = "Azshot.rss";);
             PRINT_DOC(        Psnapfile = "Psnaps.rss";);
             PRINT_DOC(        Axsnapfile = "Axsnaps.rss";);
+            PRINT_DOC(        Aysnapfile = "Aysnaps.rss";);
             PRINT_DOC(        Azsnapfile = "Azsnaps.rss";);
         }
         exit(1);
@@ -70,6 +75,7 @@ int main(int argc, char** argv) {
 	int order;
 	int snapinc;
     float apertx;
+    float aperty;
     float dtrec;
     std::string Surveyfile;
     std::string Waveletfile;
@@ -78,23 +84,29 @@ int main(int argc, char** argv) {
     bool Psnap=0, Precord=0;
     std::string Psnapfile;
     std::string Precordfile;
-    std::shared_ptr<rockseis::Data2D<float>> Pdata2D;
-    std::shared_ptr<rockseis::Data2D<float>> Pdata2Di;
+    std::shared_ptr<rockseis::Data3D<float>> Pdata3D;
+    std::shared_ptr<rockseis::Data3D<float>> Pdata3Di;
 
     bool Axsnap=0, Axrecord=0;
     std::string Axsnapfile;
     std::string Axrecordfile;
-    std::shared_ptr<rockseis::Data2D<float>> Axdata2D;
-    std::shared_ptr<rockseis::Data2D<float>> Axdata2Di;
+    std::shared_ptr<rockseis::Data3D<float>> Axdata3D;
+    std::shared_ptr<rockseis::Data3D<float>> Axdata3Di;
+
+    bool Aysnap=0, Ayrecord=0;
+    std::string Aysnapfile;
+    std::string Ayrecordfile;
+    std::shared_ptr<rockseis::Data3D<float>> Aydata3D;
+    std::shared_ptr<rockseis::Data3D<float>> Aydata3Di;
 
     bool Azsnap=0, Azrecord=0;
     std::string Azsnapfile;
     std::string Azrecordfile;
-    std::shared_ptr<rockseis::Data2D<float>> Azdata2D;
-    std::shared_ptr<rockseis::Data2D<float>> Azdata2Di;
+    std::shared_ptr<rockseis::Data3D<float>> Azdata3D;
+    std::shared_ptr<rockseis::Data3D<float>> Azdata3Di;
 
     // Create a local model class
-	std::shared_ptr<rockseis::ModelAcoustic2D<float>> lmodel;
+	std::shared_ptr<rockseis::ModelAcoustic3D<float>> lmodel;
 
     /* Get parameters from configuration file */
     std::shared_ptr<rockseis::Inparse> Inpar (new rockseis::Inparse());
@@ -114,6 +126,7 @@ int main(int argc, char** argv) {
     if(Inpar->getPar("Wavelet", &Waveletfile) == INPARSE_ERR) status = true;
     if(Inpar->getPar("Survey", &Surveyfile) == INPARSE_ERR) status = true;
     if(Inpar->getPar("apertx", &apertx) == INPARSE_ERR) status = true;
+    if(Inpar->getPar("aperty", &aperty) == INPARSE_ERR) status = true;
     if(Inpar->getPar("Psnap", &Psnap) == INPARSE_ERR) status = true;
     if(Psnap){
         if(Inpar->getPar("Psnapfile", &Psnapfile) == INPARSE_ERR) status = true;
@@ -121,6 +134,10 @@ int main(int argc, char** argv) {
     if(Inpar->getPar("Axsnap", &Axsnap) == INPARSE_ERR) status = true;
     if(Axsnap){
         if(Inpar->getPar("Axsnapfile", &Axsnapfile) == INPARSE_ERR) status = true;
+    }
+    if(Inpar->getPar("Aysnap", &Aysnap) == INPARSE_ERR) status = true;
+    if(Aysnap){
+        if(Inpar->getPar("Aysnapfile", &Aysnapfile) == INPARSE_ERR) status = true;
     }
     if(Inpar->getPar("Azsnap", &Azsnap) == INPARSE_ERR) status = true;
     if(Azsnap){
@@ -152,10 +169,10 @@ int main(int argc, char** argv) {
     Sort->setDatafile(Surveyfile);
 	
     // Create a global model class
-	std::shared_ptr<rockseis::ModelAcoustic2D<float>> gmodel (new rockseis::ModelAcoustic2D<float>(Vpfile, Rhofile, lpml ,fs));
+	std::shared_ptr<rockseis::ModelAcoustic3D<float>> gmodel (new rockseis::ModelAcoustic3D<float>(Vpfile, Rhofile, lpml ,fs));
 
     // Create a data class for the source wavelet
-	std::shared_ptr<rockseis::Data2D<float>> source (new rockseis::Data2D<float>(Waveletfile));
+	std::shared_ptr<rockseis::Data3D<float>> source (new rockseis::Data3D<float>(Waveletfile));
 
     // Create an interpolation class
     std::shared_ptr<rockseis::Interp<float>> interp (new rockseis::Interp<float>(SINC));
@@ -182,6 +199,10 @@ int main(int argc, char** argv) {
             // Create an empty data file
             Sort->createEmptydataset(Axrecordfile, ntrec, dtrec, 0.0);
         }
+        if(Ayrecord){
+            // Create an empty data file
+            Sort->createEmptydataset(Ayrecordfile, ntrec, dtrec, 0.0);
+        }
         if(Azrecord){
             // Create an empty data file
             Sort->createEmptydataset(Azrecordfile, ntrec, dtrec, 0.0);
@@ -200,8 +221,8 @@ int main(int argc, char** argv) {
     }
     else {
         /* Slave */
-        std::shared_ptr<rockseis::Data2D<float>> Shotgeom;
-        std::shared_ptr<rockseis::ModellingAcoustic2D<float>> modelling;
+        std::shared_ptr<rockseis::Data3D<float>> Shotgeom;
+        std::shared_ptr<rockseis::ModellingAcoustic3D<float>> modelling;
         while(1) {
             if(!mpi.ifActive()){
                 std::cerr << "My rank in WORLD: " << mpi.getRank() << " I am not active." << std::endl;
@@ -221,17 +242,17 @@ int main(int argc, char** argv) {
                 Sort->readKeymap();
                 Sort->readSortmap();
 
-                Shotgeom = Sort->get2DGather(work.id);
+                Shotgeom = Sort->get3DGather(work.id);
                 size_t ntr = Shotgeom->getNtrace();
-                lmodel = gmodel->getDomainmodel(Shotgeom, apertx, SMAP, mpi.getDomainrank(), mpi.getNdomain(), order);
+                lmodel = gmodel->getDomainmodel(Shotgeom, apertx, aperty, SMAP, mpi.getDomainrank(), mpi.getNdomain(), order);
                 (lmodel->getDomain())->setMpi(&mpi);
 
-                // Read wavelet data, set shot coordinates and make a map
+                // Read wavelet data, set shot and receiver coordinates and make a map
                 source->read();
                 source->copyCoords(Shotgeom);
                 source->makeMap(lmodel->getGeom(), SMAP);
 
-                modelling = std::make_shared<rockseis::ModellingAcoustic2D<float>>(lmodel, source, order, snapinc);
+                modelling = std::make_shared<rockseis::ModellingAcoustic3D<float>>(lmodel, source, order, snapinc);
 
                 // Set logfile
                 modelling->setLogfile("log.txt-" + std::to_string(work.id)+ "-" + std::to_string(mpi.getDomainrank()));
@@ -243,68 +264,45 @@ int main(int argc, char** argv) {
                 if(Axsnap){
                     modelling->setSnapAx(Axsnapfile + "-" + std::to_string(work.id)+ "-" + std::to_string(mpi.getDomainrank()));
                 }
+                if(Aysnap){
+                    modelling->setSnapAy(Aysnapfile + "-" + std::to_string(work.id)+ "-" + std::to_string(mpi.getDomainrank()));
+                }
                 if(Azsnap){
                     modelling->setSnapAz(Azsnapfile + "-" + std::to_string(work.id)+ "-" + std::to_string(mpi.getDomainrank()));
                 }
 
                 // Setting Record
                 if(Precord){
-                    Pdata2D = std::make_shared<rockseis::Data2D<float>>(ntr, source->getNt(), source->getDt(), 0.0);
-                    Pdata2D->setField(rockseis::PRESSURE);
+                    Pdata3D = std::make_shared<rockseis::Data3D<float>>(ntr, source->getNt(), source->getDt(), 0.0);
+                    Pdata3D->setField(rockseis::PRESSURE);
                     // Copy geometry to Data
-                    Pdata2D->copyCoords(Shotgeom);
-                    Pdata2D->makeMap(lmodel->getGeom(),SMAP,0,0);
-                    switch((lmodel->getDomain())->getDim()){
-                        case 0:
-                            Pdata2D->makeMap(lmodel->getGeom(),GMAP,(lmodel->getDomain())->getLpad(),0);
-                            break;
-                        case 2:
-                            Pdata2D->makeMap(lmodel->getGeom(),GMAP,0,(lmodel->getDomain())->getLpad());
-                            break;
-                        default:
-                            rs_error("mpiHellodomdec:Invalid dimension in Domain.");
-                            break;
-                    }
-                    modelling->setRecP(Pdata2D);
+                    Pdata3D->copyCoords(Shotgeom);
+                    Pdata3D->makeMap(lmodel->getGeom());
+                    modelling->setRecP(Pdata3D);
                 }
                 if(Axrecord){
-                    Axdata2D = std::make_shared<rockseis::Data2D<float>>(ntr, source->getNt(), source->getDt(), 0.0);
-                    Axdata2D->setField(rockseis::VX);
+                    Axdata3D = std::make_shared<rockseis::Data3D<float>>(ntr, source->getNt(), source->getDt(), 0.0);
+                    Axdata3D->setField(rockseis::VX);
                     // Copy geometry to Data
-                    Axdata2D->copyCoords(Shotgeom);
-                    Axdata2D->makeMap(lmodel->getGeom(),SMAP,0,0);
-                    switch((lmodel->getDomain())->getDim()){
-                        case 0:
-                            Axdata2D->makeMap(lmodel->getGeom(),GMAP,(lmodel->getDomain())->getLpad(),0);
-                            break;
-                        case 2:
-                            Axdata2D->makeMap(lmodel->getGeom(),GMAP,0,(lmodel->getDomain())->getLpad());
-                            break;
-                        default:
-                            rs_error("mpiHellodomdec:Invalid dimension in Domain.");
-                            break;
-                    }
-
-                    modelling->setRecAx(Axdata2D);
+                    Axdata3D->copyCoords(Shotgeom);
+                    Axdata3D->makeMap(lmodel->getGeom());
+                    modelling->setRecAx(Axdata3D);
+                }
+                if(Ayrecord){
+                    Aydata3D = std::make_shared<rockseis::Data3D<float>>(ntr, source->getNt(), source->getDt(), 0.0);
+                    Aydata3D->setField(rockseis::VY);
+                    // Copy geometry to Data
+                    Aydata3D->copyCoords(Shotgeom);
+                    Aydata3D->makeMap(lmodel->getGeom());
+                    modelling->setRecAy(Aydata3D);
                 }
                 if(Azrecord){
-                    Azdata2D = std::make_shared<rockseis::Data2D<float>>(ntr, source->getNt(), source->getDt(), 0.0);
-                    Azdata2D->setField(rockseis::VZ);
+                    Azdata3D = std::make_shared<rockseis::Data3D<float>>(ntr, source->getNt(), source->getDt(), 0.0);
+                    Azdata3D->setField(rockseis::VZ);
                     // Copy geometry to Data
-                    Azdata2D->copyCoords(Shotgeom);
-                    Azdata2D->makeMap(lmodel->getGeom(),SMAP,0,0);
-                    switch((lmodel->getDomain())->getDim()){
-                        case 0:
-                            Azdata2D->makeMap(lmodel->getGeom(),GMAP,(lmodel->getDomain())->getLpad(),0);
-                            break;
-                        case 2:
-                            Azdata2D->makeMap(lmodel->getGeom(),GMAP,0,(lmodel->getDomain())->getLpad());
-                            break;
-                        default:
-                            rs_error("mpiHellodomdec:Invalid dimension in Domain.");
-                            break;
-                    }
-                    modelling->setRecAz(Azdata2D);
+                    Azdata3D->copyCoords(Shotgeom);
+                    Azdata3D->makeMap(lmodel->getGeom());
+                    modelling->setRecAz(Azdata3D);
                 }
 
                 // Run modelling 
@@ -312,22 +310,28 @@ int main(int argc, char** argv) {
 
                 // Output record
                 if(Precord){
-                    Pdata2Di = std::make_shared<rockseis::Data2D<float>>(ntr, ntrec, dtrec, 0.0);
-                    Pdata2Di->setFile(Precordfile);
-                    interp->interp(Pdata2D, Pdata2Di);
-                    Sort->put2DGather(Pdata2Di, work.id, (Pdata2D->getGeom())->getGmap());
+                    Pdata3Di = std::make_shared<rockseis::Data3D<float>>(ntr, ntrec, dtrec, 0.0);
+                    Pdata3Di->setFile(Precordfile);
+                    interp->interp(Pdata3D, Pdata3Di);
+                    Sort->put3DGather(Pdata3Di, work.id);
                 }
                 if(Axrecord){
-                    Axdata2Di = std::make_shared<rockseis::Data2D<float>>(ntr, ntrec, dtrec, 0.0);
-                    Axdata2Di->setFile(Axrecordfile);
-                    interp->interp(Axdata2D, Axdata2Di);
-                    Sort->put2DGather(Axdata2Di, work.id);
+                    Axdata3Di = std::make_shared<rockseis::Data3D<float>>(ntr, ntrec, dtrec, 0.0);
+                    Axdata3Di->setFile(Axrecordfile);
+                    interp->interp(Axdata3D, Axdata3Di);
+                    Sort->put3DGather(Axdata3Di, work.id);
+                }
+                if(Ayrecord){
+                    Aydata3Di = std::make_shared<rockseis::Data3D<float>>(ntr, ntrec, dtrec, 0.0);
+                    Aydata3Di->setFile(Ayrecordfile);
+                    interp->interp(Aydata3D, Aydata3Di);
+                    Sort->put3DGather(Aydata3Di, work.id);
                 }
                 if(Azrecord){
-                    Azdata2Di = std::make_shared<rockseis::Data2D<float>>(ntr, ntrec, dtrec, 0.0);
-                    Azdata2Di->setFile(Azrecordfile);
-                    interp->interp(Azdata2D, Azdata2Di);
-                    Sort->put2DGather(Azdata2Di, work.id);
+                    Azdata3Di = std::make_shared<rockseis::Data3D<float>>(ntr, ntrec, dtrec, 0.0);
+                    Azdata3Di->setFile(Azrecordfile);
+                    interp->interp(Azdata3D, Azdata3Di);
+                    Sort->put3DGather(Azdata3Di, work.id);
                 }
 
                 // Reset all classes
@@ -335,16 +339,20 @@ int main(int argc, char** argv) {
                 lmodel.reset();
                 modelling.reset();
                 if(Precord){
-                    Pdata2D.reset();
-                    Pdata2Di.reset();
+                    Pdata3D.reset();
+                    Pdata3Di.reset();
                 }
                 if(Axrecord){
-                    Axdata2D.reset();
-                    Axdata2Di.reset();
+                    Axdata3D.reset();
+                    Axdata3Di.reset();
+                }
+                if(Ayrecord){
+                    Aydata3D.reset();
+                    Aydata3Di.reset();
                 }
                 if(Azrecord){
-                    Azdata2D.reset();
-                    Azdata2Di.reset();
+                    Azdata3D.reset();
+                    Azdata3Di.reset();
                 }
                 work.status = WORK_FINISHED;
 
